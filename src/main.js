984873ce054533449a94df4d47e78a8f6a7fa61d
Axton <peregrinator@yeah.net>
2025-05-31 00:09:21 +0800

    feat: 初步完成静态页面

--- src/main.js
+++ src/main.js
@@ -1,8 +1,1 @@
 import './style/main.css';
-
-// 示例：创建Tailwind样式的DOM元素
-const app = document.getElementById('app');
-app.innerHTML = `
-  <h1 class="text-3xl font-bold text-center mt-8 color-blue-500">Welcome to Tailwind!</h1>
-  <button class="btn-primary mt-4">点击我</button>
-`;
